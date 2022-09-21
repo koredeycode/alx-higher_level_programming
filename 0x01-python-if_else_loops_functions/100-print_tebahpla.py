917a323ce7fe90eb47cd05f23f759f2bf038e5e0
koredeycode <koredey4u@gmail.com>
2022-09-21 17:28:01 +0000

    solved pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,10 +3,6 @@ def foo(x):
     if x % 2 == 0:
         return (x)
     return(x - 32)
+
 for i in range(122, 96, -1):
-    '''
-    if i % 2 == 0:
-        print(chr(i), end="")
-    else:
-        print(chr(i - 32), end="")'''
     print("{}".format(chr(foo(i))), end="")
