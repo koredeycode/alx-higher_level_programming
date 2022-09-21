ac0f9479d68894de129908479840c5a506b72ef5
koredeycode <koredey4u@gmail.com>
2022-09-21 13:00:31 +0000

    solved pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range (0, 10):
-    for j in range (i, 10):
+for i in range(0, 10):
+    for j in range(i, 10):
         if (i != j):
-            if ( i == 8 and j == 9):
+            if (i == 8 and j == 9):
                 print("{}{}".format(i, j))
                 continue
             print("{}{}, ".format(i, j), end="")
