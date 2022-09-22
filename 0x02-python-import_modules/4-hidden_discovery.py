799413eef40b998c8424172472560ee7086fd27c
koredeycode <koredey4u@gmail.com>
2022-09-22 13:27:05 +0000

    this

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-if __na
+if __name__ == "__main__":
+    import hidden_4
+    hidlist = dir(hidden_4)
+    for i in range(0, len(hidlist)):
+        if hidlist[i][:2] == '__':
+            continue
+        print(hidlist[i])
