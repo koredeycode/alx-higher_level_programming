d1e3970de2aa2ac4186c9d0c413070e293ffab61
koredeycode <koredey4u@gmail.com>
2022-09-24 11:11:41 +0000

    this

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,9 @@
 
 def multiple_returns(sentence):
     a = len(sentence)
-    b = sentence[0]
     if len(sentence) == 0:
         b = None
+    else:
+        b = sentence[0]
     tup = (a, b)
     return (tup)
