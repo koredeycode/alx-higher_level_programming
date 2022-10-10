1b5730d412353aa3b437558215d187e7ff4f0def
koredeycode <koredey4u@gmail.com>
2022-10-10 13:16:57 +0000

    first

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	c = 0
-	try:
-		for i, v in enumerate(my_list):
-			if i < x:
-				print(v, end="")
-				c += 1
-	except IndexError:
-		pass
-	print()
-	return (c)
+    c = 0
+    try:
+        for i, v in enumerate(my_list):
+            if i < x:
+                print(v, end="")
+                c += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return (c)
