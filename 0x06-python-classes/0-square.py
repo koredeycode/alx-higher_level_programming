b07b2905cc18aec86333d67544eabc7466b481ee
koredeycode <koredey4u@gmail.com>
2022-10-11 15:11:26 +0000

    this

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module contains class Named Square.
+It is empty and does nothing"""
+
+
+class Square:
+    """This class does nothing.
+    attr:
+        No Attributes
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains project task files on Python Object Oriented Programming for alx SE course
