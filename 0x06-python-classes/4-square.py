6afdb9098a85ecff70d9b6e33dd81da2bde7f1e2
koredeycode <koredey4u@gmail.com>
2022-10-11 18:11:58 +0000

    this

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""This module contains class Named Square.
+It get instantiated with size"""
+
+
+class Square:
+    """This class does nothing.
+    Attributes:
+        __size: size of the square
+    """
+    def __init__(self, size=0):
+        """ Initialize a new Square.
+        Args:
+            size (int): the size of the squar.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        """ Return the area of a Square.
+        """
+        return (self.__size ** 2)
+
+
+    @property
+    def size(self):
+        """ Return the size  Square.
+        """
+        return (self.__size)
+
+
+    @size.setter
+    def size(self, size=0):
+        """ Change the size of a  Square.
+        Args:
+            size (int): the size of the squar.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
