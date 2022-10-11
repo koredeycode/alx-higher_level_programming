efe75865d437386f9858dc1e2669045174526d05
koredeycode <koredey4u@gmail.com>
2022-10-11 15:17:20 +0000

    this

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module contains class Named Square.
+It get instantiated with size"""
+
+
+class Square:
+    """This class does nothing.
+    Attributes:
+        __size: size of the square
+    """
+    def __init__(self, __size):
+        self.__size = __size
