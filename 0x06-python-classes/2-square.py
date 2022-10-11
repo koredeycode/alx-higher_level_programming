9446ddadbc03cab10a1dd2222affe6a833ad035d
koredeycode <koredey4u@gmail.com>
2022-10-11 16:33:23 +0000

    this

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,7 +14,7 @@ class Square:
         Args:
             size (int): the size of the squar.
         """
-        if not type(size) == type(1):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
