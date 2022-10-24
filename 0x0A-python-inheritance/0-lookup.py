0c04342c025eb9c4dbd8bf7eee730d6e1799586f
koredeycode <koredey4u@gmail.com>
2022-10-24 13:18:10 +0000

    this

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module contain the lookup function"""
+def lookup(obj):
+    """this function returns the list of available attributes and methods
+    of an object:
+    Arguments:
+        obj: the object
+    Returns: the attributes and methods of an object
+    """
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+This directory contains project task files on python inheritance for alx higer level programming for the software engineering programme
+
