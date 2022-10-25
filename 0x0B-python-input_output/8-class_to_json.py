f1e4c04ae95e0a65293e82a22be649e99d2d26c9
koredeycode <koredey4u@gmail.com>
2022-10-25 17:11:15 +0000

    this

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""this module contain the class to json function"""
+
+
+def class_to_json(obj):
+    """Return the dictionary description of the parameter"""
+    return (obj.__dict__)
