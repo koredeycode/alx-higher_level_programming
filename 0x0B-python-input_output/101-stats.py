6453f69cffcfcd83435cecefef5ade784e9cf5ab
koredeycode <koredey4u@gmail.com>
2022-10-25 21:01:30 +0000

    this

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+def print_stats(size, stat):
+    """Print the stats infos"""
+    print("File size: {}".format(size))
+    for k in sorted(stat):
+        print("{}: {}".format(k, stat[k]))
+
+with open(0, "r", encoding="utf-8") as std:
+    fsize = 0
+    dictt = {
+                "200": 0,
+                "301": 0,
+                "400": 0,
+                "401": 0,
+                "403": 0,
+                "404": 0,
+                "405": 0,
+                "500": 0
+            }
+    count = 0
+    try:
+        for line in std:
+            if count == 10:
+                print_stats(fsize, dictt)
+                count = 1
+            else:
+                count += 1
+            li = line.split()
+            try:
+                fsize += int(li[-1])
+            except (IndexError, ValueError):
+                pass
+            try:
+                if li[-2] in dictt.keys():
+                    if dictt.get(li[-2], -1) == -1:
+                        dictt[li[-2]] = 1
+                    else:
+                        dictt[li[-2]] += 1
+            except IndexError:
+                pass
+        print_stats(fsize, dictt)
+    except KeyboardInterrupt:
+        print_stats(fsize, dictt)
+        raise
