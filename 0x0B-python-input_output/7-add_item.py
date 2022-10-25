811b5e5f73590cd7db16c6ba451ac93b46729991
koredeycode <koredey4u@gmail.com>
2022-10-25 16:27:28 +0000

    this

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """This module contains the add_item function"""
 import sys
-save = __import__("5-save_to_json_file").save_to_json_file
-load = __import__("6-load_from_json_file").load_from_json_file
+if __name__ == "__main__":
+    save = __import__("5-save_to_json_file").save_to_json_file
+    load = __import__("6-load_from_json_file").load_from_json_file
 
-args = sys.argv[1:]
+    args = sys.argv[1:]
 
-try:
-    items = load("add_item.json")
-except FileNotFoundError:
-    items = []
-items.extend(args)
-save(items, "add_item.json")
+    try:
+        items = load("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(args)
+    save(items, "add_item.json")
