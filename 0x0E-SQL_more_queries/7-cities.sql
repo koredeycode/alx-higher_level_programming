2ab2a180154ff5647148090c97659bc630e070e5
koredeycode <koredey4u@gmail.com>
2022-12-01 20:55:29 +0000

    7done

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create database and table
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities` (
+	state_id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL, 
+	PRIMARY KEY(id),
+	FOREIGN KEY(state_id) REFERENCES state(id)
+);
