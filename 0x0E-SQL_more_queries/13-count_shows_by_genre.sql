0f92f01e4ba7e1c71b11b15d9fa8920ebf67b238
koredeycode <koredey4u@gmail.com>
2022-12-02 12:08:19 +0000

    this

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- count show by genre id
+
+SELECT tg.name, COUNT(*) AS number_of_shows FROM tv_genres AS tg INNER JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id GROUP BY tg.name ORDER BY number_of_shows DESC;
