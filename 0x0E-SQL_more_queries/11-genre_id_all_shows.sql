8a2670ed71508f54b643fda70b080e501e752c95
koredeycode <koredey4u@gmail.com>
2022-12-02 12:04:29 +0000

    this

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- list all shows contained in the database
+
+SELECT ts.title, tsg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id ORDER BY ts.title, tsg.genre_id;
