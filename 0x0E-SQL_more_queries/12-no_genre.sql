723c5716f148125e13772a7fd7822376105d4b8f
koredeycode <koredey4u@gmail.com>
2022-12-02 12:06:30 +0000

    done

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,2 @@
+-- show show with no genre linked
+SELECT ts.title, tsg.genre_id FROM tv_shows AS ts LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id WHERE tsg.genre_id IS NULL ORDER BY ts.title, tsg.genre_id;
