fd169e8e443e14e563444643ca0b36e6c174b76e
koredeycode <koredey4u@gmail.com>
2022-12-02 12:09:56 +0000

    this

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,2 @@
+-- List the genre of 'dexter'
+SELECT tv_genres.name FROM tv_shows INNER JOIN tv_show_genres INNER JOIN tv_genres ON tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name;
