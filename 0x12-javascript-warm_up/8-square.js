17719152f502dde2783dcace7273e480319ac1c8
koredeycode <koredey4u@gmail.com>
2022-12-19 13:37:01 +0000

    this

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const count = parseInt(process.argv[2]);
+  let i = 0;
+  while (i < count) {
+    console.log('X'.repeat(count));
+    i += 1;
+  }
+}
