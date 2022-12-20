27f07db82b3b5afd0128abb5d14ade0cfacb8adc
koredeycode <koredey4u@gmail.com>
2022-12-20 13:05:02 +0000

    this

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i += 1;
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,3 @@
+# Javascript_objects_scopes_closures
+In this project, I learned about Javascript_objects_scopes_closures as part of my projects for the ALX Software Engineering Programme
+* Koredeycode
