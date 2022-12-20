eff12f489e9c6c236bdae3f2f38b771b861aaeb4
koredeycode <koredey4u@gmail.com>
2022-12-20 13:19:50 +0000

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareParent = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareParent {
   constructor (size) {
     super(size, size);
   }
