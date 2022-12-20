752f1a4e1afd2565f866f2e02846b56ebe0e241b
koredeycode <koredey4u@gmail.com>
2022-12-20 21:08:27 +0000

    this

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,17 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-console.log(dict);
+const values = Object.values(dict);
+const entries = Object.entries(dict);
+const uniqValues = [...new Set(values)];
+const newObj = {};
+for (const i in uniqValues) {
+  const li = [];
+  for (const j in entries) {
+    if (entries[j][1] === uniqValues[i]) {
+      li.push(entries[j][0]);
+	}
+  }
+  newObj[uniqValues[i]] = li;
+}
+console.log(newObj);
