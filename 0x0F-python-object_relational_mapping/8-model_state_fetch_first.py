8f69ed9ecfc2c201a27b070f6567afaa07081952
koredeycode <koredey4u@gmail.com>
2022-12-21 19:17:09 +0000

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,17 +5,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import sys
 
-db_url = 'mysql+mysqldb://{}:{}@localhost/{}' \
-        .format(sys.argv[1], sys.argv[2], sys.argv[3])
-engine = create_engine(db_url)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-query = session.query(State).order_by(State.id).all()
+if __name__ == "__main__":
+    db_url = 'mysql+mysqldb://{}:{}@localhost/{}' \
+            .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).order_by(State.id).all()
 
-q = None if query is None else query[0]
-if q is not None:
-    print("{}: {}".format(q.id, q.name))
-else:
-    print("Nothing")
-session.close()
+    q = None if query is None else query[0]
+    if q is not None:
+        print("{}: {}".format(q.id, q.name))
+    else:
+        print("Nothing")
+    session.close()
