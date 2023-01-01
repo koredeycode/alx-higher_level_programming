0339b451f74b4ec3747894660a54cc2885708995
koredeycode <koredey4u@gmail.com>
2023-01-01 19:43:56 +0000

    cleanup

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Contains the class definition of a State and a Base instance"""
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
