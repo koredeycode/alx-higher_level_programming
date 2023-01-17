8a0861e2c84063f19b8f3d5d0ece5119108c34d9
koredeycode <koredey4u@gmail.com>
2023-01-17 22:36:43 +0000

    this

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+"""This module contain a funtion that find a peak in a list"""
+
+
+def find_peak(lint):
+    """find a peak in a list"""
+
+    n = len(lint)
+    if n < 3:
+        return None
+    mid = n // 2
+    if n == 1:
+        return lint[0]
+    if n == 2:
+        return max(lint)
+    if lint[mid] >= max(lint[mid - 1], lint[mid + 1]):
+        return lint[mid]
+    if lint[mid] < lint[mid + 1]:
+        return find_peak(lint[mid:])
+    if lint[mid] < lint[mid - 1]:
+        return find_peak(lint[:mid])
