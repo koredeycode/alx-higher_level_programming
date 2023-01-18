db80e2f6c55c856145860daf6fc80f1112721298
koredeycode <koredey4u@gmail.com>
2023-01-18 15:35:43 -0800

    pycodestyle

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module do something"""
 import urllib.request
-import sys 
+import sys
 url = sys.argv[1]
 request = urllib.request.Request(url)
 with urllib.request.urlopen(request) as response:
