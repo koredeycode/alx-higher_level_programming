e2e00403b74da24a16480b5cb257ce1fa9e4a457
koredeycode <koredey4u@gmail.com>
2023-01-18 16:30:44 -0800

    this

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,7 @@ import requests
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with requests.get(url) as response:
-        content = response.content
+        content = response.text
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode()))
