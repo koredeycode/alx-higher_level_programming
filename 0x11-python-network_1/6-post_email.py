5f96d04ee6e2131a437596535b36b4a63ffa1704
koredeycode <koredey4u@gmail.com>
2023-01-18 16:40:42 -0800

    this

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module do something"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {}
+    values["email"] = sys.argv[2]
+    with requests.post(url, data=values) as response:
+        content = response.content
+        print("{}".format(content))
