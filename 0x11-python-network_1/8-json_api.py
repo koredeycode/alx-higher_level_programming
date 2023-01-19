5e3ae345a1d049d69fbff7d05eb8088dfacee538
koredeycode <koredey4u@gmail.com>
2023-01-18 17:24:04 -0800

    thus

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module do something"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    values = {}
+    try:
+        values["q"] = sys.argv[1]
+    except IndexError:
+        values["q"] = ""
+    with requests.post(url, data=values) as response:
+        obj = response.json()
+        if not obj:
+            print("No result")
+        elif type(obj) is not dict:
+            print("Not a valid JSON")
+        else:
+            print("[{}] {}".format(obj.get("id"), obj.get("name")))
